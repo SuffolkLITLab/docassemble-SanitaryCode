77ec9d89fd605bf55896ac20e249659fe728c535
Michael Cronin <65620548+mikecronin7@users.noreply.github.com>
2020-12-01 21:23:34 -0500

    second commit

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.SanitaryCode',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.SanitaryCode\n\nA docassemble extension.\n\n## Author\n\nMichael Cronin, mcronin@su.suffolk.edu\n\n',
+      long_description='# docassemble.SanitaryCode\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMichael Cronin, mcronin@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Michael Cronin',
       author_email='mcronin@su.suffolk.edu',
